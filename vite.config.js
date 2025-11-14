08999753223ae38e150359c3a7cf76f1046b3570
Khalil-Kouroghli <mohamedkhalil.kouroghli@gmail.com>
2025-11-14 13:13:03 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      port: 3001,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true
+        },
+        '/uploads': {
+          target: env.VITE_API_URL || 'http://localhost:3000',
+          changeOrigin: true
+        }
       }
     }
-  }
+  };
 });
 
